a0cc62b5760f840eff0b9fd2759e091c89073ded
satoo_hiro <h.s.nagano.t@gmail.com>
2021-09-20 18:45:38 +0900

    header完了

--- functions.php
+++ functions.php
@@ -9,25 +9,23 @@
         }
             return $title;
         }
-    add_filter( 'pre_get_document_title', 'hamburger_title' ); //
-    function hamburger_script() {
+    add_filter( 'pre_get_document_title', 'hamburger_title' ); //function hamburger_script() {
         wp_enqueue_style( 'font-awesome', '"//use.fontawesome.com/releases/v5.6.1/css/all.css"', array(), '5.6.1' );
         wp_enqueue_style( 'hamburger', get_template_directory_uri() . '/scss/hamburger.css', array(), '1.0.0' );
         wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0.0' );
         wp_enqueue_script( 'toggle', get_template_directory_uri() . '/js/Hamburger.js', array('jquery'), '1.0.0', true);
-        
+        function hamburger_widgets_init() {
+            register_sidebar (
+                array(
+                    'name'          => 'All Menu',
+                    'id'            => 'menu_widget',
+                    'description'   => 'メニューの一覧です',
+                    'before_widget' => '<div id="%1$s" class="widget %2$s">',
+                    'after_widget'  => '</div>',
+                    'before_title'  => '<h2 class="c-category-title">',
+                    'after_title'   => "</h2>\n",
+                )
+            );
+        add_action( 'widgets_init', 'hamburger_widgets_init' );
+        add_action( 'wp_enqueue_scripts', 'hamburger_script' );
     }
-    function hamburger_widgets_init() {
-        register_sidebar (
-            array(
-                'name'          => 'All Menu',
-                'id'            => 'menu_widget',
-                'description'   => 'メニューの一覧です',
-                'before_widget' => '<div id="%1$s" class="widget %2$s">',
-                'after_widget'  => '</div>',
-                'before_title'  => '<h2 class="c-category-title">',
-                'after_title'   => "</h2>\n",
-            )
-        );
-    add_action( 'widgets_init', 'hamburger_widgets_init' );
-    add_action( 'wp_enqueue_scripts', 'hamburger_script' );
