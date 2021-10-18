8c27b9023494a307dd4bb11af1cac63f33a63282
satoo_hiro <h.s.nagano.t@gmail.com>
2021-10-19 00:36:38 +0900

    エラー解決　hamburger_script

--- functions.php
+++ functions.php
@@ -14,7 +14,6 @@ add_filter( 'pre_get_document_title', 'hamburger_title' ); //function hamburger_
     wp_enqueue_style( 'hamburger', get_template_directory_uri() . '/scss/hamburger.css', array(), '1.0.0' );
     wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0.0' );
     wp_enqueue_script( 'toggle', get_template_directory_uri() . '/js/Hamburger.js', array('jquery'), '1.0.0', true);
-    add_action( 'wp_enqueue_scripts', 'hamburger_script' );
     function hamburger_widgets_init() {
         register_sidebar (
             array(
