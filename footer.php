fbd716bfb71ea54ed2ba9c3908f2f5d7fc88ae12
satoo_hiro <h.s.nagano.t@gmail.com>
2021-10-25 00:14:30 +0900

    footer追加

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="l-footer">
+  <div class="l-footer__center">
+    <li><a href="#">ショップ情報</a></li>
+    <hr class="line__border"></p>
+    <li><a href="#">ヒストリー</li></a>
+  </div>
+  <div class="l-footer__copyright">
+    <p>Copyright: RaiseTech</p>
+  </div>
+</footer>
--- index.php
+++ index.php
@@ -49,15 +49,6 @@
     </article>
   <?php get_sidebar(); ?> 
  </div>
- <footer class="l-footer">
-   <div class="l-footer__center">
-     <li><a href="#">ショップ情報</a></li>
-     <hr class="line__border"></p>
-     <li><a href="#">ヒストリー</li></a>
-    </div>
-    <div class="l-footer__copyright">
-      <p>Copyright: RaiseTech</p>
-    </div>
-  </footer>
+ 
   <?php wp_footer(); ?>
 </body>
