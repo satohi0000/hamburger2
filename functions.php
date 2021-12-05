2eda798d471a209d253602fb82a64f08c5861bfc
satoo_hiro <h.s.nagano.t@gmail.com>
2021-12-05 23:33:57 +0900

    サイドバーにメニューを加えたい

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@ function hamburger_script(){
     wp_enqueue_style( 'hamburger', get_template_directory_uri() . '/css/hamburger.css', array(), '1.0.0' );
     wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0.0' );
     wp_enqueue_script( 'toggle', get_template_directory_uri() . '/js/Hamburger.js', array('jquery'), '1.0.0', true);
-    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery-3.6.0.js', "", "3.5.1", true );
+    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery-3.6.0.js', "", "3.6.0", true );
 }
 add_action( 'wp_enqueue_scripts', 'hamburger_script' );
 
@@ -27,11 +27,11 @@ function hamburger_widgets_init() {
             'name'          => 'All Menu',
             'id'            => 'menu_widget',
             'description'   => 'メニューの一覧です',
-            'before_widget' => '<div>',
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
             'after_widget'  => '</div>',
-            'before_title'  => '<h2>',
+            'before_title'  => '<h2 class="c-category-title">',
             'after_title'   => "</h2>\n",
-            )
-        );
+        )
+    );
 }
 add_action( 'widgets_init', 'hamburger_widgets_init' );
--- sidebar.php
+++ sidebar.php
@@ -1,42 +1,18 @@
 <aside class="l-contents__right">
     <div class="l-sidebar">
-        <span class="l-sidebar__batsu"></span>
+        <span class="l-sidebar__batsu">
+        </span>
         <a href="#">Menu</a>
-        <ul>
-            <li>
-                <a href="#">バーガー</a>
-                <ul>
-                    <li><a href="#">ハンバーガー</a></li>
-                    <li><a href="#">チーズバーガー</a></li>
-                    <li><a href="#">テリヤキバーガー</a></li>
-                    <li><a href="#">アボカドバーガー</a></li>
-                    <li><a href="#">フィッシュバーガー</a></li>
-                    <li><a href="#">ベーコンバーガー</a></li>
-                    <li><a href="#">チキンバーガー</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">サイド</a>
-                <ul>
-                    <li><a href="#">ポテト</a></li>
-                    <li><a href="#">サラダ</a></li>
-                    <li><a href="#">ナゲット</a></li>
-                    <li><a href="#">コーン</a></li>
-                </ul>
-            </li> 
-            <li>
-                <a href="#">ドリンク</a>
-                <ul>
-                    <li><a href="#">コーラ</a></li>
-                    <li><a href="#">ファンタ</a></li>
-                    <li><a href="#">オレンジ</a></li>
-                    <li><a href="#">アップル</a></li>
-                    <li><a href="#">紅茶（Ice/Hot）</a></li>
-                    <li><a href="#">コーヒー（Ice/Hot）</a></li>
-                </ul>
-            </li>
-       </ul>
+        <?php
+            if ( is_active_sidebar( 'menu_widget' ) ) :
+                dynamic_sidebar( 'menu_widget' );
+            else:
+        ?>
+        <div class="widget">
+            <h2>No Widget</h2>
+            <p>ウィジットは設定されていません。</p>
+        </div>
+        <?php endif; ?>
     </div>
 </aside>
 
-<?php dynamic_sidebar( 'sidebar' ); ?>
