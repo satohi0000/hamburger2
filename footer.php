2466e6aa62adcb85ab9da6c409deae8fcfc49a6b
satoo_hiro <h.s.nagano.t@gmail.com>
2021-12-07 00:19:53 +0900

    footer 完成

--- footer.php
+++ footer.php
@@ -1,10 +1,14 @@
 <footer class="l-footer">
   <div class="l-footer__center">
-    <li><a href="#">ショップ情報</a></li>
+    <li><a href=<?php echo get_page_link(53); ?>>ショップ情報</a></li>
     <hr class="line__border"></p>
-    <li><a href="#">ヒストリー</li></a>
+    <li><a href=<?php echo get_page_link(50); ?>>ヒストリー</li></a>
   </div>
   <div class="l-footer__copyright">
     <p>Copyright: RaiseTech</p>
   </div>
+  
 </footer>
+<?php wp_footer(); ?>
+</body>
+</html>
