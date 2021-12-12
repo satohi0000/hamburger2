9cfe8e931194f39a0259c877f3e1a4d384d4c0b3
satoo_hiro <h.s.nagano.t@gmail.com>
2021-12-12 14:57:54 +0900

    page編集　script削除

--- page.php
+++ page.php
@@ -4,112 +4,26 @@
       <div class="l-contents__left__page__top">
         <div class="l-contents__left__page__top__img"></div>
         <div class="l-contents__left__page__top__word">
-          <h1>ショップについて</h1>
+          <h1><?php the_title(); ?></h1>
         </div>
       </div>
-      <div class="l-contents__left__single__h2">
-        <h2>見出しh２</h2>
-      </div>
-      <div class="l-contents__left__single__p">
-        <p>Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。</p>
-      </div>
-      <div class="l-contents__left__single__h3">
-        <h3>見出しh3</h3>
-      </div>
-      <div class="l-contents__left__single__h4">
-        <h4>見出しh4</h4>
-      </div>
-      <div class="l-contents__left__single__h5">
-        <h5>見出しh5</h5>
-      </div>
-      <div class="l-contents__left__single__gray">
-        <p>Blockquote 引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ</p>
-        <p>出典元：<a href="#">○○○○○○○○○○○○</a></p>
-      </div>
-      <div class="l-contents__left__single__burger__img"></div>
-      <div class="l-contents__left__single__burger__set">
-        <div class="l-contents__left__single__burger__set__img"></div>
-        <div class="l-contents__left__single__burger__set__sentence">
-          <p> テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります</p>
+      <?php if(have_posts()): //もし投稿データがあったら
+      while(have_posts()): //以下を繰り返してくださいね
+      the_post(); ?> 
+      <div id="post-<?php the_ID(); ?>"<?php post_class(); ?>> 
+            <?php the_content(); ?>
         </div>
-      </div>
-      <div class="l-contents__left__single__burger__set">
-        <div class="l-contents__left__single__burger__set__sentence">
-          <p> テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります</p>
-        </div>
-        <div class="l-contents__left__single__burger__set__img"></div>
-      </div>
-      <div class="l-contents__left__single__burger__set__img__big"></div>
-      <div class="l-contents__left__single__burger__set__images"></div>
-      <div class="l-contents__left__single__list">
-      <div class="l-contents__left__single__list__1">
-        <p>1.リストリストリスト<br>2.リストリストリスト</p>
-      </div>
-      <div class="l-contents__left__single__list__2">
-        <p>1.リスト2リスト2リスト<br>2.リスト2リスト2リスト</p>
-      </div>
-      <div class="l-contents__left__single__list__1">
-        <p>1.リストリストリスト<br>2.リストリストリスト</p>
-      </div>
-    </div>
-    <div class="l-contents__left__single__list">
-      <div class="l-contents__left__single__list__1">
-        <p>リストリストリスト<br>リストリストリスト</p>
-      </div>
-      <div class="l-contents__left__single__list__2">
-        <p>リスト2リスト2リスト<br>リスト2リスト2リスト</p>
-      </div>
-      <div class="l-contents__left__single__list__1">
-        <p>リストリストリスト<br>リストリストリスト</p>
-      </div>
-    </div>
-    <pre class="l-contents__left__single__code">
-      &lt;html&gt;
-          &lt;head&gt;
-          &lt;/head&gt;
-          &lt;body&gt;
-          &lt;/body&gt;
-     &lt;/html&gt;
-    </pre>
-    <table class="l-contents__left__single__table">
-      <tr>
-        <th>テーブル</th>
-        <td>テーブル</td>
-      </tr>
-      <tr>
-        <th>テーブル</td>
-        <td>テーブル</td>
-      </tr>
-      <tr>
-        <th>テーブル</th>
-        <td>テーブル</td>
-      </tr>
-      <tr>
-        <th>テーブル</th>
-        <td>テーブル</td>
-      </tr>
-    </table>
-    <div class="l-contents__left__single__switch">
-      <a href="">ボタン</a>
-    </div>
-    <div class="l-contents__left__single__bold">
-      <p>boldboldboldboldboldboldbold</p>
-    </div> 
+    <?php endwhile; //繰り返しここまでです。まだデータがあるならwhileに戻ってね
+    else: //投稿がなければ、
+    ?>
+    <p>表示する記事がありません</p>
+    <?php endif; ?>
+</div>
   </article>
   <?php get_sidebar(); ?> 
 </div>
-<footer class="l-footer">
-  <div class="l-footer__center">
-    <li><a href="#">ショップ情報</a></li>
-    <hr class="line__border"></p>
-    <li><a href="#">ヒストリー</li></a>
-  </div>
-  <div class="l-footer__copyright">
-    <p>Copyright: RaiseTech</p>
-  </div>
-</footer>
-<script src="js/jquery-3.6.0.js"></script>
-<script src="js/Hamburger.js"></script>
+<<?php get_footer(); ?>
+
 </body>  
   
 
