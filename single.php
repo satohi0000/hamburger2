2a92330c1882ec3d0380b87ad7e554f68ed70ea5
satoo_hiro <h.s.nagano.t@gmail.com>
2022-01-24 01:32:18 +0900

    single page 大枠完成

--- single.php
+++ single.php
@@ -1,25 +1,22 @@
-<?php get_header(); ?>    
-<div class="p-article">
-  <article class="p-article__left">
-    <div class="p-article__left__single__top">
-      <div class="p-article__left__single__top__word">
-        <h1><?php the_title(); ?></h1>
+<?php get_header(); ?> 
+  <div class="p-article">
+    <article class="p-article__left">
+      <div class="p-article__left__single__top">
+        <a href="#">
+          <?php the_title(); ?>
+        </a>
       </div>
-      <div class="p-article__left__single__top__img">
-        
+      <div class="p-article__left__single__bottom">
+        <?php if(have_posts()): 
+        while(have_posts()):
+        the_post();?>
       </div>
+      <div id="post-<?php the_ID(); ?>"<?php post_class(); ?><?php the_content(); ?>>
+      <?php endwhile;else: ?>
+        <p>表示する記事がありません</p>
+      <?php endif; ?>
     </div>
-    <?php if(have_posts()): while(have_posts()):the_post(); ?>
-    <div id="post-<?php the_ID(); ?>"<?php post_class(); ?>> 
-     <?php the_content(); ?>
-    </div>
-    <?php endwhile;
-    else: 
-    ?>
-    <p>表示する記事がありません</p> 
-    <?php endif; ?>
   </article>
-    <?php get_sidebar(); ?>
+  <?php get_sidebar(); ?> 
 </div>
 <?php get_footer(); ?>
-
