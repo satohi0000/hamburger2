9aaa08280f01bb3205c325b49e42291bc87f3c41
satoo_hiro <h.s.nagano.t@gmail.com>
2022-01-24 13:00:11 +0900

    archive.phpが反映されない

--- functions.php
+++ functions.php
@@ -35,3 +35,14 @@ function hamburger_widgets_init() {
     );
 }
 add_action( 'widgets_init', 'hamburger_widgets_init' );
+
+// 投稿のアーカイブページを作成する
+function post_has_archive($args, $post_type)
+{
+    if ('post' == $post_type) {
+        $args['rewrite'] = true; // リライトを有効にする
+        $args['has_archive'] = 'blog'; // 任意のスラッグ名
+    }
+    return $args;
+}
+add_filter('register_post_type_args', 'post_has_archive', 10, 2);
