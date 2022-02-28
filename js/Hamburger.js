48fb619676a4d678b120adaaa0104f7dc55e87b9
satoo_hiro <h.s.nagano.t@gmail.com>
2022-02-28 14:27:11 +0900

    コンテンツスクロール固定

--- js/Hamburger.js
+++ js/Hamburger.js
@@ -1,9 +1,11 @@
 jQuery(".l-header__left__menu__botton").click(function () {
   jQuery(".l-sidebar").toggleClass("__is-open");
   jQuery(".l-sidebar__batsu").toggleClass("__is-open")
+  jQuery(".l-contents").toggleClass("__is-open")
 });
 
 jQuery(".l-sidebar__batsu").click(function () {
   jQuery(".l-sidebar").toggleClass("__is-open");
   jQuery(".l-sidebar__batsu").toggleClass("__is-open")
+  jQuery(".l-contents").toggleClass("__is-open")
 });
