454a4cc8f7c9fdacea7e769784262a2dd6476cee
satoo_hiro <h.s.nagano.t@gmail.com>
2022-03-21 18:17:44 +0900

    スクロールバー　ボタン　訂正完了

--- js/Hamburger.js
+++ js/Hamburger.js
@@ -1,8 +1,9 @@
 jQuery(".l-header__left__menu__botton").click(function () {
   jQuery(".l-sidebar").toggleClass("__is-open");
   jQuery(".l-sidebar2").toggleClass("__is-open");
-  jQuery(".l-sidebar__batsu").toggleClass("__is-open")
-  jQuery(".l-contents").toggleClass("__is-open")
+  jQuery(".l-sidebar__batsu").toggleClass("__is-open");
+  jQuery(".l-contents").toggleClass("__is-open");
+  jQuery("body").css("overflow-y","hidden");
 });
 
 jQuery(".l-sidebar__batsu").click(function () {
@@ -10,4 +11,5 @@ jQuery(".l-sidebar__batsu").click(function () {
   jQuery(".l-sidebar2").toggleClass("__is-open");
   jQuery(".l-sidebar__batsu").toggleClass("__is-open")
   jQuery(".l-contents").toggleClass("__is-open")
+  jQuery("body").css("overflow-y","auto");
 });
