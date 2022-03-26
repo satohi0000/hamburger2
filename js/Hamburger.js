d5d108b4b9fd3b7b29fcaa333bc40d8bc7101b18
satoo_hiro <h.s.nagano.t@gmail.com>
2022-03-26 16:43:13 +0900

    JS変更

--- js/Hamburger.js
+++ js/Hamburger.js
@@ -1,31 +1,30 @@
-$(".l-header__left__menu__botton").click(function () {
-  $(".l-sidebar").toggleClass("__is-open");
-  $(".l-sidebar2").toggleClass("__is-open");
-  $(".l-sidebar__batsu").toggleClass("__is-open");
-  $(".l-contents").toggleClass("__is-open");
-  $("body").css("overflow-y","hidden");
+jQuery(".l-header__left__menu__botton").click(function () {
+  jQuery(".l-sidebar").toggleClass("__is-open");
+  jQuery(".l-sidebar2").toggleClass("__is-open");
+  jQuery(".l-sidebar__batsu").toggleClass("__is-open");
+  jQuery(".l-contents").toggleClass("__is-open");
+  jQuery("body").css("overflow-y","hidden");
 });
 
-$(".l-sidebar__batsu").click(function () {
-  $(".l-sidebar").toggleClass("__is-open");
-  $(".l-sidebar2").toggleClass("__is-open");
-  $(".l-sidebar__batsu").toggleClass("__is-open")
-  $(".l-contents").toggleClass("__is-open")
-  $("body").css("overflow-y","auto");
+jQuery(".l-sidebar__batsu").click(function () {
+  jQuery(".l-sidebar").toggleClass("__is-open");
+  jQuery(".l-sidebar2").toggleClass("__is-open");
+  jQuery(".l-sidebar__batsu").toggleClass("__is-open")
+  jQuery(".l-contents").toggleClass("__is-open")
+  jQuery("body").css("overflow-y","auto");
 });
 
-$(function(){
-  // 高さの最大値を代入するための変数h
+jQuery(function(){
+  
   var h = 0;
-
-  // それぞれの要素の高さを調べる
-  $("l-main__under__map__box").each(function(){
-    // 要素の高さと変数hの値を比較し、大きい方を変数hに代入
-    if($(this).height() > h){
-      h = $(this).height();
-    }
+  jQuery("l-main__under__map__box") //コメントを入力してあるクラス
+  
+  .each(function(){
+  if($(this).height() > h){
+        h = jQuery(this).overheight();
+      }
+    });
+    
+    jQuery("l-main__under__map__img")　 //地図のクラス
+  .css("height", h + "px");
   });
-
-  // 要素の高さを変数hの値に統一します
-  $("l-main__under__map__img").css("height", h + "px");
-});
