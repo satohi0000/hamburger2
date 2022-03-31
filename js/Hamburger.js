527388cad3a08252170d4da16a23a6c86d8e7e96
satoo_hiro <h.s.nagano.t@gmail.com>
2022-03-31 15:04:28 +0900

    地図が広がらない

--- js/Hamburger.js
+++ js/Hamburger.js
@@ -9,22 +9,14 @@ jQuery(".l-header__left__menu__botton").click(function () {
 jQuery(".l-sidebar__batsu").click(function () {
   jQuery(".l-sidebar").toggleClass("__is-open");
   jQuery(".l-sidebar2").toggleClass("__is-open");
-  jQuery(".l-sidebar__batsu").toggleClass("__is-open")
-  jQuery(".l-contents").toggleClass("__is-open")
+  jQuery(".l-sidebar__batsu").toggleClass("__is-open");
+  jQuery(".l-contents").toggleClass("__is-open");
   jQuery("body").css("overflow-y","auto");
 });
 
-jQuery(function(){
-  
-  var h = 0;
-  jQuery("l-main__under__map__box") //コメントを入力してあるクラス
-  
-  .each(function(){
-  if($(this).height() > h){
-        h = jQuery(this).overheight();
-      }
-    });
-    
-    jQuery("l-main__under__map__img")　 //地図のクラス
-  .css("height", h + "px");
-  });
+$(function(){
+var height = jQuery(".l-main__under__map__box").outerWidth(true);
+var width = jQuery(".l-main__under__map__box").outerHeight(true);
+jQuery("l-main__under__map").css(width,"width");
+jQuery("l-main__under__map").css(height,"height");
+});
